38f0756992d0ba78a7106c0cd451a440aa2ce446
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-09 10:02:10 -0300

    terminado

--- utils/Firebase.js
+++ utils/Firebase.js
@@ -1,10 +1,22 @@
-export default (firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  });
-  
+import { initializeApp } from "firebase/app";
+import {getStorage, ref, uploadBytes ,getDownloadURL} from 'firebase/storage'
+import {v4} from 'uuid'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkgBHdLw1FsAZ1xaQ5dAxdLRoW30DqXvg",
+  authDomain: "course-scope-398003.firebaseapp.com",
+  projectId: "course-scope-398003",
+  storageBucket: "course-scope-398003.appspot.com",
+  messagingSenderId: "343392987610",
+  appId: "1:343392987610:web:b351e2222a9a5fdfd7f8e3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
+
+ export async function uploadfile(file){
+  const storagRef =ref(storage, v4())
+  await uploadBytes(storagRef, file)
+  const url = await getDownloadURL(storagRef)
+  return url
+}
